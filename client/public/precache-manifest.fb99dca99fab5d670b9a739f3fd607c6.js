58070a09854815505ed38fe8809b48cb88b25af7
Akilas Yemane <ayemane@gmail.com>
2019-05-19 03:40:53 -0400

    .

---
+++ client/public/precache-manifest.fb99dca99fab5d670b9a739f3fd607c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a483e745a11beba29440d5ed9b8f62d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ecf539321a32b19ebbd2",
+    "url": "/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "9befd25d32551fcece18",
+    "url": "/static/js/2.dbb8316e.chunk.js"
+  },
+  {
+    "revision": "ecf539321a32b19ebbd2",
+    "url": "/static/js/main.322d5d1d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "63af327efc488862f2576a33889a922f",
+    "url": "/static/media/logo.63af327e.jpg"
+  }
+]);
