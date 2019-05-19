182729b830b3e2d5078eba5850c581159b577a8d
Akilas Yemane <ayemane@gmail.com>
2019-05-19 03:51:20 -0400

    .

--- api/server.js
+++ api/server.js
@@ -17,5 +17,7 @@ app.use(
   })
 );
 
+app.use(express.static(__dirname + "client/build"));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
